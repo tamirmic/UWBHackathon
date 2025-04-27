df28ebe4699f71c42ec4bca077ad198581a5b447
Gjones747 <gsman707@gmail.com>
2025-04-27 03:08:08 -0700

    tufff

--- server/ai_typefein.js
+++ server/ai_typefein.js
@@ -9,7 +9,7 @@ console.log(API_KEY)
 const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const formatPrompt = (object) => {
-    return `You are a high level consulting agent, but for small restaurants. Read the following customer reviews and give specific actionable feedback on what the bussiness should do to best improve and increase profit. Please format the response in short sections headed by bold text. In each section include description of the issue along with a bulleted list of actionable imrovable items. Please keep each section breif! Here are the reviews "${object}"`;
+    return `You are a high level consulting agent, but for small restaurants. Read the following customer reviews and give specific actionable feedback on what the bussiness should do to best improve and increase profit. Please format the response in short sections headed by bold text. In each section include description of the issue along with a bulleted list of actionable imrovable items. Try to limit whitespace in response. Please keep each section breif! Here are the reviews "${object}"`;
 };
 
 const geminiResponse = async (object) => {
